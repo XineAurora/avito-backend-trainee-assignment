5fab95f5167cdcded5eb5db9299964bef6d2fe27
XineAurora <qwerty-20010628@yandex.ru>
2023-08-29 23:34:43 +0400

    Removed soft deletion

--- internal/models/segment.go
+++ internal/models/segment.go
@@ -5,21 +5,12 @@ import (
 )
 
 type Segment struct {
-	gorm.Model
+	ID   uint   `gorm:"primarykey"`
 	Name string `gorm:"unique;not null"`
 }
 
 func CreateSegment(db *gorm.DB, name string) (*Segment, error) {
 	segment := Segment{Name: name}
-	// if segment was deleted before, restore it
-	if err := db.Unscoped().Where("name=?", name).First(&segment).Error; err == nil && segment.DeletedAt.Valid {
-		err = db.Unscoped().Model(&segment).Update("deleted_at", nil).Error
-		if err != nil {
-			return nil, err
-		}
-		return &segment, nil
-	}
-	//
 	if err := db.Create(&segment).Error; err != nil {
 		return nil, err
 	}
